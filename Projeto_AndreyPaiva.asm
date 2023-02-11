e98215bb9722117cf377c7eadebfc90904bbe469
Andrey Paiva <andrey.paiva5@gmail.com>
2023-02-10 21:17:39 -0300

    agr loopa certo

--- Projeto_AndreyPaiva.asm
+++ Projeto_AndreyPaiva.asm
@@ -1,13 +1,14 @@
 .data
 	Matriculas: .word 0x20111766, 0x19110394, 0x19110236, 0x19111026, 0x19110324, 0x20210674, 0x19110386, 0x18110269, 0x20110520, 0x20111601, 0x18210553, 0x19110420, 0x19111418, 0x19110904, 0x18110318, 0x18210651, 0x19110524, 0x19111014, 0x19110191, 0x19111664, 0x18210469, 0x19111299, 0x19110929, 0x19111150, 0x19210570, 0x19110421, 0x19110734, 0x18210809, 0x22111275, 0x18110402, 0x18210499, 0x22110406, 0x19110177, 0x20110415, 0x20111610, 0x20111401, 0x19110415, 0x19210861, 0x20111044, 0x21111310, 0x19110030, 0x20110266, 0x19111203, 0x17210743, 0x19110376, 0x19110390, 0x20210121, 0x19110779, 0x19110423, 0x18111101, 0x21110599, 0x19111090, 0x18210114, 0x19110785, 0x20110898, 0x20210436, 0x19110104, 0x20210601, 0x21111274, 0x19111128
-	Nomes: .asciiz "ADEILSON DE SOUSA LEAL                       ", "AIRON CIRILO GUENES FILHO                    ", "ALYSSON MACHADO DE OLIVEIRA BARBOSA          ", "ANDERSON MARCOS SILVA                        ", "ANDREY LUCYAN NASCIMENTO PAIVA               ", "BARBARA BEZERRA BARBOSA                      ", "BRUNO NASCIMENTO GOMES DE OLIVEIRA           ", "CAIO VASCONCELOS BENIGNO DE ABRANTES         ", "CLEISSON DE ALENCAR RAMOS                    ", "DAVI AUGUSTO MONTEIRO GONZAGA                ", "DEBORA DA SILVA COSTA                        ", "DIEGO ALMEIDA DIAS                           ", "DOUGLAS BARROS CALISTA                       ", "EDGLEY DA SILVA CARVALHO FILHO               ", "EDUARDO LUIS FERREIRA BARBOSA                ", "EUDES MEDEIROS                               ", "FABIO VICTOR GALDINO NASCIMENTO              ", "FELIPE ALEXANDRE DA SILVA BENTO              ", "FELIPE LUCENA SOUZA MEDEIROS                 ", "FERNANDO HENRIQUE DE SOUSA SILVA             ", "FILIPE JOSE FERREIRA DE ARAUJO               ", "FRANCINILDO BARBOSA FIGUEIREDO               ", "HEBERT SANTOS CRISPIM                        ", "HELSON JADER ROCHA                           ", "HIGOR COSTA DE OLIVEIRA                      ", "IGOR COSTA XAVIER                            ", "IURY CHAGAS DA SILVA CAETANO                 ", "JACKSON DA SILVA GUEDES                      ", "JANIELE PEREIRA BENICIO                      ", "JESSYCA DAYANE SOUSA BRANDAO                 ", "JOSE HELIO DE ARAUJO JUNIOR                  ", "JOSE TAYRONE SANTOS DE OLIVEIRA              ", "JULIA RAMALHO COSTA SOUZA                    ", "KAYO MATHEUS ARAUJO SANTOS                   ", "LAVNIS OLIVEIRA DE ARAUJO                    ", "LEIRY GABRIELLE MARQUES LUZ PINTO            ", "LEONARDO PESSOA BANDEIRA LACERDA             ", "LINDEMBERG COSTA LUNA                        ", "LUCAS ALLYSON HERMINIO SOUSA                 ", "LUCAS CARMO PASCHOAL                         ", "LUCAS DE OLIVEIRA LOBO                       ", "LUCAS HENRIQUE ANDRADE VILA NOVA             ", "MARCUS VINICIUS COSTA PEREIRA                ", "MARIA THEREZA GOMES DO NASCIMENTO ROQUE      ", "MICAEL ESPINOLA FONSECA TOMAZ                ", "MOISES DE ARAUJO OLIVEIRA                    ", "PEDRO HENRIQUE FERNANDES MACEDO              ", "PEDRO LIRA BANDEIRA                          ", "RAFAEL SA BENIGNO                            ", "RAFAELA TAVARES MARTINS                      ", "ROGERIO MOREIRA ALMEIDA                      ", "ROSMANYA MARLLA FIALHO DE LIMA FONSECA       ", "SABRINA ARAUJO CARDOSO                       ", "SAMARA LIMA CARDOSO                          ", "TULIO RAFAEL DE AGUIAR TAVARES               ", "UBIRAJARA POSSIANO DA SILVA FILHO            ", "VITOR TRINDADE ROCHA RIBEIRO                 ", "VITORIA JAMILLE GONCALVES DE OLIVEIRA NOBREGA", "WALISSON MAXIMINO MESSIAS                    ", "WILLIAM HENRIQUE AZEVEDO MARTINS             "
+	Nomes: .asciiz "ADEILSON DE SOUSA LEAL                       \n", "AIRON CIRILO GUENES FILHO                    \n", "ALYSSON MACHADO DE OLIVEIRA BARBOSA          \n", "ANDERSON MARCOS SILVA                        \n", "ANDREY LUCYAN NASCIMENTO PAIVA               \n", "BARBARA BEZERRA BARBOSA                      \n", "BRUNO NASCIMENTO GOMES DE OLIVEIRA           \n", "CAIO VASCONCELOS BENIGNO DE ABRANTES         \n", "CLEISSON DE ALENCAR RAMOS                    \n", "DAVI AUGUSTO MONTEIRO GONZAGA                \n", "DEBORA DA SILVA COSTA                        \n", "DIEGO ALMEIDA DIAS                           \n", "DOUGLAS BARROS CALISTA                       \n", "EDGLEY DA SILVA CARVALHO FILHO               \n", "EDUARDO LUIS FERREIRA BARBOSA                \n", "EUDES MEDEIROS                               \n", "FABIO VICTOR GALDINO NASCIMENTO              \n", "FELIPE ALEXANDRE DA SILVA BENTO              \n", "FELIPE LUCENA SOUZA MEDEIROS                 \n", "FERNANDO HENRIQUE DE SOUSA SILVA             \n", "FILIPE JOSE FERREIRA DE ARAUJO               \n", "FRANCINILDO BARBOSA FIGUEIREDO               \n", "HEBERT SANTOS CRISPIM                        \n", "HELSON JADER ROCHA                           \n", "HIGOR COSTA DE OLIVEIRA                      \n", "IGOR COSTA XAVIER                            \n", "IURY CHAGAS DA SILVA CAETANO                 \n", "JACKSON DA SILVA GUEDES                      \n", "JANIELE PEREIRA BENICIO                      \n", "JESSYCA DAYANE SOUSA BRANDAO                 \n", "JOSE HELIO DE ARAUJO JUNIOR                  \n", "JOSE TAYRONE SANTOS DE OLIVEIRA              \n", "JULIA RAMALHO COSTA SOUZA                    \n", "KAYO MATHEUS ARAUJO SANTOS                   \n", "LAVNIS OLIVEIRA DE ARAUJO                    \n", "LEIRY GABRIELLE MARQUES LUZ PINTO            \n", "LEONARDO PESSOA BANDEIRA LACERDA             \n", "LINDEMBERG COSTA LUNA                        \n", "LUCAS ALLYSON HERMINIO SOUSA                 \n", "LUCAS CARMO PASCHOAL                         \n", "LUCAS DE OLIVEIRA LOBO                       \n", "LUCAS HENRIQUE ANDRADE VILA NOVA             \n", "MARCUS VINICIUS COSTA PEREIRA                \n", "MARIA THEREZA GOMES DO NASCIMENTO ROQUE      \n", "MICAEL ESPINOLA FONSECA TOMAZ                \n", "MOISES DE ARAUJO OLIVEIRA                    \n", "PEDRO HENRIQUE FERNANDES MACEDO              \n", "PEDRO LIRA BANDEIRA                          \n", "RAFAEL SA BENIGNO                            \n", "RAFAELA TAVARES MARTINS                      \n", "ROGERIO MOREIRA ALMEIDA                      \n", "ROSMANYA MARLLA FIALHO DE LIMA FONSECA       \n", "SABRINA ARAUJO CARDOSO                       \n", "SAMARA LIMA CARDOSO                          \n", "TULIO RAFAEL DE AGUIAR TAVARES               \n", "UBIRAJARA POSSIANO DA SILVA FILHO            \n", "VITOR TRINDADE ROCHA RIBEIRO                 \n", "VITORIA JAMILLE GONCALVES DE OLIVEIRA NOBREGA\n", "WALISSON MAXIMINO MESSIAS                    \n", "WILLIAM HENRIQUE AZEVEDO MARTINS             \n"
 	Comando: .asciiz "Digite sua matrícula: "
 	Inicio: .asciiz "A matrícula 1"
 	Acerto: .asciiz " corresponde ao aluno "
-	Erro: .asciiz " não foi encontrada"
-	Eu: .asciiz "Sou eu! Eu sou ANDREY LUCYAN NASCIMENTO PAIVA, e tenho matrícula 119110324"
+	Erro: .asciiz " não foi encontrada\n"
+	Eu: .asciiz "Sou eu! Eu sou ANDREY LUCYAN NASCIMENTO PAIVA, e tenho matrícula 119110324\n"
 	
 .text
+Start:
 	li $v0, 4			#Carrega 4 em $v0, para que a operação de exibir uma string seja realizada
 	la $a0, Comando			#Carrega a string a ser exibida em $a0
 	syscall				#Exibe a string
@@ -77,7 +78,7 @@ Conversor:				#Converte o hexadecimal fornecido para decimal
 Hit:					#Executa a rotina de acerto
 	beq $t8, 4, Meu			#Caso o contador de repetições aponte para o meu nome, executar a rotina correspondente
 	
-	mul $t9, $t8, 46		#Multiplica o contador salvo em $t8 por 46 para servir de índice do array de nomes
+	mul $t9, $t8, 47		#Multiplica o contador salvo em $t8 por 46 para servir de índice do array de nomes
 	li $v0, 4			#Carrega 4 em $v0, para que a operação de exibir uma string seja realizada
 	la $a0, Inicio			#Carrega a string a ser exibida em $a0
 	syscall				#Exibe a string
@@ -94,14 +95,14 @@ Hit:					#Executa a rotina de acerto
 	la $a0, Nomes($t9)		#Carrega a string a ser exibida em $a0
 	syscall				#Exibe a string
 	
-	j exit				#Encerra o programa
+	j Start				#Reinicia o programa
 	
 Meu:					#Exibe a mensagem de que a matrícula é a minha
 	li $v0, 4			#Carrega 4 em $v0, para que a operação de exibir uma string seja realizada
 	la $a0, Eu			#Carrega a string a ser exibida em $a0
 	syscall				#Exibe a string	
 	
-	j exit				#Encerra o programa
+	j Start				#Reinicia o programa
 
 Miss:					#Executa a rotina de erro
 	li $v0, 4			#Carrega 4 em $v0, para que a operação de exibir uma string seja realizada
@@ -116,7 +117,7 @@ Miss:					#Executa a rotina de erro
 	la $a0, Erro			#Carrega a string a ser exibida em $a0
 	syscall				#Exibe a string
 	
-	j exit
+	j Start				#Reinicia o programa
 	
 exit:					#Executa a rotina de encerramento
 	li $v0, 10			#Carrega 10 em $v0, para que a operação de encerrar a execução seja realizada
